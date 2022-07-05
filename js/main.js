322923a8a89eefd711d0351901bb9e9f5e9e4300
Enzo <enzocarabajal7513@gmail.com>
2022-07-05 18:30:57 -0300

    Prueba conect

--- js/main.js
+++ js/main.js
@@ -1,31 +1,72 @@
 // Ejemplo clase 1
-let nombre = prompt("ingrese su nombre")
-let apellido = prompt("ingrese su apellido")
-alert ("Bienvenido a Mendoza Viajes " + nombre + " " + apellido)
+// let nombre = prompt("ingrese su nombre")
+// let apellido = prompt("ingrese su apellido")
+// alert ("Bienvenido a Mendoza Viajes " + nombre + " " + apellido)
 
 //ejemplo clase 2
-let nombreUsuario = prompt("Ingresar nombre de usuario");
-if (nombreUsuario == "") {
-    alert("No puedes acceder a descuentos");
-}
-else {
-    alert("Acá podrás ver todos los descuentos " + nombreUsuario);
-}
-let ingreso = 1
-if(ingreso == 1){
-    console.log("Estás en la página principal")
-}
-if(ingreso == 2){
-    console.log("No puedes ingresar")
-}
+// let nombreUsuario = prompt("Ingresar nombre de usuario");
+// if (nombreUsuario == "") {
+//     alert("No puedes acceder a descuentos");
+// }
+// else {
+//     alert("Acá podrás ver todos los descuentos " + nombreUsuario);
+// }
+// let ingreso = 1
+// if(ingreso == 1){
+//     console.log("Estás en la página principal")
+// }
+// if(ingreso == 2){
+//     console.log("No puedes ingresar")
+// }
 //ejemplo clase 3
-let numero = 1
-while(numero < 10){
-    console.log(numero)
-    numero = numero + 1
-}
+// let numero = 1
+// while(numero < 10){
+//     console.log(numero)
+//     numero = numero + 1
+// }
 
 
 
 
 
+//clase 5
+//function Persona(nombre, edad) {
+   // //constructor
+   // this.nombre = nombre;
+   // this.edad = edad;
+   // this.piernas = 4;
+  
+    //funciones
+    //this.hablar = function () {
+    //  console.log('hola yo soy ' + (this.nombre || 'amigo') + ' tengo ' + this.piernas + ' piernas!');
+    //};
+    //this.resetear = function () {
+    //  this.nombre = '';
+    //  this.edad = 0;
+    //  this.piernas = 0;
+    //};
+  //}
+  
+  class Persona {
+    //constructor
+    constructor(nombre, edad) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.piernas = 4;
+    }
+    //funciones
+    hablar() {
+      console.log('hola yo soy ' + (this.nombre || 'amigo') + ' tengo ' + this.piernas + ' piernas!');
+    }
+    resetear() {
+      this.nombre = '';
+      this.edad = 0;
+      this.piernas = 0;
+    }
+  }
+  
+  const persona1 = new Persona('Homero', 39);
+  persona1.resetear();
+  persona1.hablar();
+
+  
