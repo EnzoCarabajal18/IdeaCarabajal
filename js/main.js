b9df2a6078bf5bd11a9fe79ad0bb5a7d15af0f94
Enzo <enzocarabajal7513@gmail.com>
2022-07-12 17:43:16 -0300

    Nueva función con Array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+let ubicacion = ["marDelPlata", "bariloche", "jujuy", "villaGesell"];
+let precio = [34.535, 65.535, 57.535, 39.535];
+let destino = "El destino seleccionado es: ";
+function agregardestino(){
+  let elegir = prompt("Seleccione el destino:\n1 - Mar del Plata\n2 - Bariloche\n3 - Jujuy\n4 - Villa Gesell\n5 - Salir");
+  if(elegir == 5){
+    console.log("Cerraste el programa");
+  }
+  else if(5 > elegir){
+    for (let i = 0; i < elegir; i++){
+      if(ubicacion[i] == ubicacion[elegir-1]){
+        alert("Elegiste "+ubicacion[elegir-1]+" por solo $: "+precio[elegir-1]);
+      }
+    }
+  }
+  else{
+    alert("Opción incorrecta!!!");
+  }
+}
+agregardestino();
 // Ejemplo clase 1
 // let nombre = prompt("ingrese su nombre")
 // let apellido = prompt("ingrese su apellido")
@@ -119,18 +139,78 @@
 //   alert = prompt("Ingrese cantidad de habitaciones")
 // }
 
-let cantHabitacionesTotal = prompt("Cantidad de habitaciones disponibles");
-let precioFinde = 10000;
-let pregunta;
-console.log("La cantidad de habitaciones disponibles es de " + cantHabitacionesTotal);
-let gananciaTotal = 0;
-for (let habitaciones = 1; habitaciones <= cantHabitacionesTotal; habitaciones++) {
-  pregunta = prompt("La habitación "+ habitaciones + " se ocupó? elige una opción:\n1 - Si \n2 - No");
-  if(pregunta == 1){
-    console.log("ocupada");
-    gananciaTotal = gananciaTotal + precioFinde
-  }else {
-    console.log("no ocupada");
-  }
-}
-alert ("La ganancia del fin de semana fue de $" + gananciaTotal);
+//ejemplo terminado
+
+// let cantHabitacionesTotal = prompt("Cantidad de habitaciones disponibles");
+// let precioFinde = 10000;
+// let pregunta;
+// console.log("La cantidad de habitaciones disponibles es de " + cantHabitacionesTotal);
+// let gananciaTotal = 0;
+// for (let habitaciones = 1; habitaciones <= cantHabitacionesTotal; habitaciones++) {
+//   pregunta = prompt("La habitación "+ habitaciones + " se ocupó? elige una opción:\n1 - Si \n2 - No");
+//   if(pregunta == 1){
+//     console.log("ocupada");
+//     gananciaTotal = gananciaTotal + precioFinde
+//   }else {
+//     console.log("no ocupada");
+//   }
+// }
+// alert ("La ganancia del fin de semana fue de $" + gananciaTotal);
+
+
+
+// function Persona(nombre, edad, calle) {
+//   this.nombre = nombre;
+//   this.edad   = edad;
+//   this.calle  = calle;
+//   this.hablar = function(){ console.log("Hola soy "+ this.nombre + ", tengo "+ this.edad + " años.")}
+// }
+// const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+// const persona2 = new Persona("Marge", 36, "Av. Siempreviva 742");
+// persona1.hablar();
+//persona2.hablar();
+// class Persona{
+//   constructor(nombre, edad, calle) {
+//       this.nombre = nombre;
+//       this.edad   = edad;
+//       this.calle  = calle;
+//   }
+//   hablar (){
+//     console.log("Hola soy "+ this.nombre + ", tengo "+ this.edad + " años y vivo en "+ this.calle)
+//   }
+// }
+// const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+// persona1.hablar();
+
+//ejemplo profe
+// for (const producto of productos) {
+//   let contenedor = document.createElement('div');
+//   //Definimos el innerHTML del elemento con una plantilla de texto
+//   contenedor.innerHTML = `<h3> ID: ${producto.id}</h3>
+//                             <p>  Producto: ${producto.nombre}</p>
+//                             <b> $ ${producto.precio}</b>`;
+//   document.body.appendChild(contenedor);
+// }
+
+
+// class Producto {
+//   constructor(ubicacion, precio) {
+//       this.nombre  = nombre;
+//       this.precio  = precio;
+//       this.alquilado = false;
+//   }
+//   sumaIva() {
+//       this.precio = this.precio * 1.21;
+//   }
+// }
+// //Declaramos un array de productos para almacenar objetos
+// const productos = [];
+// productos.push(new Producto("arroz", "125"));
+// productos.push(new Producto("fideo", "70"));
+// productos.push(new Producto("pan", "50"));
+// //Iteramos el array con for...of para modificarlos a todos
+// for (const producto of productos)
+//   producto.sumaIva();
+//   console.log(productos);
+
+
