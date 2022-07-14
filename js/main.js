8d18bb721c0915635d84066048752b62c94e7345
Enzo <enzocarabajal7513@gmail.com>
2022-07-14 14:57:49 -0300

    conjugando clases

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese su apellido")
+alert ("Bienvenido " + nombre + " " + apellido + " a Mendoza Viajes");
 let ubicacion = ["Mar Del Plata", "bariloche", "jujuy", "villa Gesell"];
 const descuentos = ubicacion.slice (0, 1);
 const descuentos2 = ubicacion.slice (3, 4);
@@ -25,5 +28,18 @@ agregardestino();
 
 
 
-
-
+// let cantHabitacionesTotal = prompt("Cantidad de habitaciones disponibles");
+// let precioFinde = 10000;
+// let pregunta;
+// console.log("La cantidad de habitaciones disponibles es de " + cantHabitacionesTotal);
+// let gananciaTotal = 0;
+// for (let habitaciones = 1; habitaciones <= cantHabitacionesTotal; habitaciones++) {
+//   pregunta = prompt("La habitación "+ habitaciones + " se ocupó? elige una opción:\n1 - Si \n2 - No");
+//   if(pregunta == 1){
+//     console.log("ocupada");
+//     gananciaTotal = gananciaTotal + precioFinde
+//   }else {
+//     console.log("no ocupada");
+//   }
+// }
+// alert ("La ganancia del fin de semana fue de $" + gananciaTotal);
