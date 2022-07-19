fdc8b4e5d9f4d017ab0400d99674d2f858ce4fb3
Enzo <enzocarabajal7513@gmail.com>
2022-07-19 14:49:47 -0300

    Interactuando con HTML

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-let nombre = prompt("ingrese su nombre");
-alert ("Bienvenido " + nombre + " "+ " a Mendoza Viajes");
-let ubicacion = ["Mar Del Plata", "bariloche", "jujuy", "villa Gesell"];
-const descuentos = ubicacion.slice (0, 1);
-const descuentos2 = ubicacion.slice (3, 4);
-alert ("Por temporada baja " + descuentos + " y " + descuentos2 +" están en descuento");
-let precio = [34.535, 65.535, 57.535, 39.535];
-let destinoElegido,precioElegido;
-agregardestino();
-function agregardestino(){
-  let elegir = prompt("Seleccione el destino:\n1 - Mar del Plata\n2 - Bariloche\n3 - Jujuy\n4 - Villa Gesell\n5 - Salir");
-  if(elegir == 5){
-    alert("Cerraste el programa");
-  }
-  else if((5 > elegir) && (elegir > 0)){
-    for (let i = 0; i < elegir; i++){
-      if(ubicacion[i] == ubicacion[elegir-1]){
-        destinoElegido = ubicacion[i];
-        precioElegido = precio[i];
-      }
-    }
-  }
-  else{
-    alert("Opción incorrecta!!!");
-  }
-}
-
-function Lugar(nombreLugar, precioLugar) {
-  this.nombreLugar = nombreLugar;
-  this.precioLugar = precioLugar;
-  this.informacion = function(){ alert("La ciudad "+ this.nombreLugar +" tiene un costo de "+ this.precioLugar)}
-}
-const lugar1 = new Lugar(destinoElegido,precioElegido);
-if(destinoElegido){
-  lugar1.informacion();
-}
 function accion(){
   let desplaza = document.getElementsByClassName('menu');
   for(let i = 0; i < desplaza.length; i++){
@@ -41,4 +5,51 @@ function accion(){
   }
 }
 
+let parrafo = document.getElementsByClassName('text-2');
+parrafo[3].textContent="Ingresá a nuestra app y obtené beneficios increibles";
 
+
+let container = document.getElementById("contenedor")
+container.innerHTML = "<p> Incluye impuestos, tasas y cargos</p>"
+
+let texto = document.querySelector(".col-12 col-md-6 #contenedor2")
+contenedor2.textContent= "25% OFF SOLO POR HOY";
+
+
+
+// let nombre = prompt("ingrese su nombre");
+// alert ("Bienvenido " + nombre + " "+ " a Mendoza Viajes");
+// let ubicacion = ["Mar Del Plata", "bariloche", "jujuy", "villa Gesell"];
+// const descuentos = ubicacion.slice (0, 1);
+// const descuentos2 = ubicacion.slice (3, 4);
+// alert ("Por temporada baja " + descuentos + " y " + descuentos2 +" están en descuento");
+// let precio = [34.535, 65.535, 57.535, 39.535];
+// let destinoElegido,precioElegido;
+// agregardestino();
+// function agregardestino(){
+//   let elegir = prompt("Seleccione el destino:\n1 - Mar del Plata\n2 - Bariloche\n3 - Jujuy\n4 - Villa Gesell\n5 - Salir");
+//   if(elegir == 5){
+//     alert("Cerraste el programa");
+//   }
+//   else if((5 > elegir) && (elegir > 0)){
+//     for (let i = 0; i < elegir; i++){
+//       if(ubicacion[i] == ubicacion[elegir-1]){
+//         destinoElegido = ubicacion[i];
+//         precioElegido = precio[i];
+//       }
+//     }
+//   }
+//   else{
+//     alert("Opción incorrecta!!!");
+//   }
+// }
+
+// function Lugar(nombreLugar, precioLugar) {
+//   this.nombreLugar = nombreLugar;
+//   this.precioLugar = precioLugar;
+//   this.informacion = function(){ alert("La ciudad "+ this.nombreLugar +" tiene un costo de "+ this.precioLugar)}
+// }
+// const lugar1 = new Lugar(destinoElegido,precioElegido);
+// if(destinoElegido){
+//   lugar1.informacion();
+// }
